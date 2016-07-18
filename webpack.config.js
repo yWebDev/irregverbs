67c9ccea3a307f0938ea0f1777e4a470f940410f
Юрий <tdev066@gmail.com>
2016-07-18 22:59:26 +0300

    build app

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require("path");
 module.exports = {
     entry: './src/js/app.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'js/bundle.js'
     },
     devtool: 'source-map',
