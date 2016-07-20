a28dd3a5852efe04ebb2633080daf1f8eb92dce6
Юрий <tdev066@gmail.com>
2016-07-20 22:23:18 +0300

    fix procfile and index.js

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use('/', express.static(path.resolve(__dirname, 'public')));
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(3000, function () {
-   console.log('Server was started at port 3000');
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Server was started at port ' + app.get('port'));
 });
