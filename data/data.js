b053536a97650d6e161b01e21a958fee1cb2ff88
Юрий <tdev066@gmail.com>
2016-07-23 22:37:33 +0300

    start create markup

--- data/data.js
+++ data/data.js
@@ -1,20 +1,20 @@
 /**
  * Created by Юрий on 20.07.2016.
  */
-exports.verbsData = {
-    1: [
+exports.verbsData = [
+    [
         'be',
         'was, were',
         'been'
     ],
-    2: [
+    [
         'beat',
         'beat',
         'beaten'
     ],
-    3: [
+    [
         'become',
         'became',
         'become'
     ]
-};
+];
--- public/src/js/app.js
+++ public/src/js/app.js
@@ -4,3 +4,10 @@
 var bootstrap = require("bootstrap-webpack");
 var less = require("../less/custom-styles.less");
 var jQuery = require("jquery");
+(function ($) {
+    $(document).ready(function () {
+        $('#search-verb').on('input', function () {
+
+        });
+    });
+})(jQuery);
